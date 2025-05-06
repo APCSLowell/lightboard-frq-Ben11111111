f73b6af5cf6be19ae691eed0f3551e0003b92b30
Ben11111111 <ben042554@gmail.com>
2025-05-06 08:07:31 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -1,48 +1,38 @@
 public class LightBoard
 {
-  /** The lights on the board, where true represents on and false represents off.
-   */
   private boolean[][] lights;
 
-  /** Constructs a LightBoard object having numRows rows and numCols columns.
-   * Precondition: numRows > 0, numCols > 0
-   * Postcondition: each light has a 40% probability of being set to on.
-   */
   public LightBoard(int numRows, int numCols)
   {
     lights = new boolean[numRows][numCols];
-    for(int r = 0; r < numRows; r++)
+    for (int r = 0; r < numRows; r++)
     {
-      for(int c = 0; c < numCols; c++)
+      for (int c = 0; c < numCols; c++)
       {
         lights[r][c] = Math.random() < 0.4;
       }
     }
   }
 
-  /** Evaluates a light in row index row and column index col and returns a status
-   *  as described in part (b).
-   *  Precondition: row and col are valid indexes in lights.
-   */
   public boolean evaluateLight(int row, int col)
   {
     int numOn = 0;
-    for(int r = 0; r < lights.length; r++)
+    for (int r = 0; r < lights.length; r++) 
     {
-      if(lights[r][col])
+      if (lights[r][col])
       {
         numOn++;
       }
     }
-    if(lights[row][col] && numOn % 2 == 0)
+    if (lights[row][col] && numOn % 2 == 0)
     {
       return false;
     }
-    if(!lights[row][col] && numOn % 3 == 0)
+    if (!lights[row][col] && numOn % 3 == 0)
     {
       return true;
     }
-    return lights[row][col];
+    return lights[row][col];  
   }
 
   public boolean[][] getLights()
@@ -50,7 +40,6 @@ public class LightBoard
     return lights;
   }
 
-  //used for testing
   public String toString()
   {
     String s = "";
